92a93883a0414cc93f3fdde740dc92f67926ec1b
javier <javiermontes1997@hotmail.com>
2020-10-14 00:41:30 +0200

    Estado de error del email

--- js/index.js
+++ js/index.js
@@ -4,12 +4,24 @@ const messageError = document.getElementById("message-error");
 formNewsletter.addEventListener("submit", (event) => {
   const email = event.target.email;
   new RegExp(/\S+@\S+\.\S+/).test(email.value)
-    ? (email.value = "")
+    ? clearForm(email)
     : showErrorMessage();
   event.preventDefault();
 });
-
+function clearForm(email) {
+  email.value = "";
+  messageError.className = "message-error__text--not-show";
+  formNewsletter.email.className = "newsletter__field-email";
+  formNewsletter
+    .querySelector(".newsletter__group")
+    .classList.remove("newsletter__group--error");
+}
 function showErrorMessage() {
-  messageError.classList.remove("not-show");
-  messageError.classList.add("show");
+  messageError.classList.remove("message-error__text--not-show");
+  messageError.classList.add("message-error__text");
+  formNewsletter.email.classList.remove("newsletter__field-email");
+  formNewsletter.email.classList.add("newsletter__field-email--border-red");
+  formNewsletter
+    .querySelector(".newsletter__group")
+    .classList.add("newsletter__group--error");
 }
